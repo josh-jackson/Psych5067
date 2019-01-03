24c0117a639258f5f53d453cd1c4a8b8d548de0e
Josh Jackson <j.jackson@wustl.edu>
2019-01-03 15:37:46 -0600

    new syllabus!

---
+++ takehome-1a.R
@@ -0,0 +1,26 @@
+---
+title: "Takehome 1a ANOVA"
+output: html_document
+---
+  
+  
+1. plot the data, using an appropriate plot
+
+2. Run a t-test. Interpret each important part of the output. 
+
+3. Run a one-way ANOVA. Interpret each part of the output. 
+
+4. Run a linear model (regression) with dummy coding (the default in R). 
+a. Interpret each part of the output.  
+b. What is similiar to your t-testa and ANOVA? 
+c. What is different? 
+d. what would happen if your IV was coded as a numeric (ie metric or continous or interval, etc) variable as opposed to a factor (nominal variable)?
+e. Recode your IV as an ordinal variable. What does this change?   
+
+5. Run a linear model with effects coding. How does this differ from the linear model in #4?
+
+6. Run a linear model with more than two groups as a predictor. Run an ANOVA with the same variables. Compare and contrast difference remembering to use both the summary and the anova functions 
+
+7. Save the residuals from the model in 6 as a new variable in a dataframe. 
+a. what are the descriptive stats of this new variable?
+b. For subject 5, show how the 1)model coefficients, 2) the participant's residual and 3) the participant's IV and DV create the regression equation 
